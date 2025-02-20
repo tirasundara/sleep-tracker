af7b30c65220f77feda66625a6954fed964f85a4
Tira Sundara <sundaralinus@gmail.com>
2025-02-21 01:05:28 +0700

    Define association between User and SleepRecord

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
+  has_many :sleep_records, dependent: :destroy
+
   validates :name, presence: true
+
+  def active_sleep_record
+    sleep_records.active.order(created_at: :desc).first
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,26 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  it { should have_many(:sleep_records).dependent(:destroy) }
+
   it { should validate_presence_of(:name) }
+
+  describe '#active_sleep_record' do
+    let(:user) { create(:user) }
+
+    it 'returns the active sleep record' do
+      completed_record = create(:sleep_record, :completed, user: user)
+      active_record = create(:sleep_record, user: user)
+
+      expect(user.active_sleep_record).to eq(active_record)
+    end
+
+    it 'returns nil when no active sleep record' do
+      create(:sleep_record, :completed, user: user)
+
+      expect(user.active_sleep_record).to be_nil
+    end
+  end
 end
