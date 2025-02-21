df4a72701c86e8a24b7c0a3eb1a800a822cee751
Tira Sundara <sundaralinus@gmail.com>
2025-02-21 10:57:14 +0700

    Established User and Following relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,32 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  # Sleep Record relationships
   has_many :sleep_records, dependent: :destroy
+
+  # Follower relationships
+  has_many :active_followings, class_name: "Following", foreign_key: "follower_id", dependent: :destroy
+  has_many :following, through: :active_followings, source: :followed
+
+  # Followed relationships
+  has_many :passive_followings, class_name: "Following", foreign_key: "followed_id", dependent: :destroy
+  has_many :followers, through: :passive_followings, source: :follower
 
   validates :name, presence: true
 
   def active_sleep_record
     sleep_records.active.order(created_at: :desc).first
+  end
+
+  def follow(other_user)
+    following << other_user unless self == other_user
+  end
+
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,10 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it { should have_many(:sleep_records).dependent(:destroy) }
+  it { should have_many(:active_followings).dependent(:destroy) }
+  it { should have_many(:following) }
+  it { should have_many(:passive_followings).dependent(:destroy) }
+  it { should have_many(:followers) }
 
   it { should validate_presence_of(:name) }
 
@@ -21,6 +25,33 @@ RSpec.describe User, type: :model do
       create(:sleep_record, :completed, user: user)
 
       expect(user.active_sleep_record).to be_nil
+    end
+  end
+
+  describe '#follow' do
+    let(:user) { create(:user) }
+    let(:other_user) { create(:user) }
+
+    it 'follows another user' do
+      user.follow(other_user)
+      expect(user.following?(other_user)).to be true
+    end
+
+    it 'does not follow self' do
+      user.follow(user)
+      expect(user.following?(user)).to be false
+    end
+  end
+
+  describe '#unfollow' do
+    let(:user) { create(:user) }
+    let(:other_user) { create(:user) }
+
+    before { user.follow(other_user) }
+
+    it 'unfollows a user' do
+      user.unfollow(other_user)
+      expect(user.following?(other_user)).to be false
     end
   end
 end
